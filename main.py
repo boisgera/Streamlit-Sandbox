9c690beb64055a1cb7fd65df3055ec7a53c9312f
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-04-22 07:19:11 +0200

    primitive constructs exploration

--- main.py
+++ main.py
@@ -3,6 +3,120 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+# ------------------------------------------------------------------------------
+
+st.title("Magic")
+
+s = "This is **magic**"
+s
+
+"I can tell"
+
+"Let's try some other Python objects"
+
+
+
+### Mmm how do I display a string OBJECT with the quotes then?
+
+t = repr(s)
+t
+
+repr(s) # doesn't show
+repr(s) + "" # does show. Lol. Magic is quite britle ...
+
+b"BYTE\x00Z"
+
+st.header("Numbers")
+
+1
+
+1.0
+
+1.0j
+
+
+st.markdown("**Vector** is Displayed as a *column* matrix ... urk.")
+
+x = np.zeros(6)
+x
+
+"**Row vector.** Displayed as a row vector (matrix). Here makes sense."
+
+st.write(np.zeros((1, 6)))
+
+"**Matrix**"
+
+A = np.ones((2,3))
+A
+
+"**Tensor**"
+
+T = np.ones((2, 3 , 4))
+T
+
+st.header("Containers")
+
+st.subheader("Lists")
+
+[1, 2, 3]
+
+[1, 2, [3, 4, [5]]]
+
+st.subheader("Dictionary")
+
+'Let\'s see what `{"a": 5, "b": 8, 9: "caramba!", "dict": {0: 2, 1: 3}, "list": [2,3]}` looks like.'
+
+{"a": 5, "b": 8, 9: "caramba!", "dict": {0: 2, 1: 3}, "list": [2,3]}
+
+"""
+Dafuq. Streamlits quotes my integer keys! And reorder my key-value pairs!!!
+**BAD!**
+"""
+
+"Ah! Here tuple value converted as list: `{'struct': (1,1,1)}`"
+
+{"struct": (1,1,1)}
+
+"""
+But `{(0,1): 42}` won't work, because it want to display something that it can
+convert to JSON. Oh my! 
+"""
+
+{(0,1): 42}
+
+'''
+The number-to-string conversion can be fucked up quite easily, see e.g.
+`{99: 99, "z": 1, "a": 2, 0: 0, "0": "'0'", "1": True}`
+'''
+
+{99: 99, "z": 1, "a": 2, 0: 0, "0": "'0'", "1": True}
+
+"""
+Note that keys that can be converted to numbers are reorders ... but not the
+string keys (???)
+"""
+
+st.subheader("Tuples")
+
+"Literal tuple"
+
+(6, 6, "skid", "🐤") # Note the same !!!
+
+"Tuple assigned to variable, then display variable:", (6, 6, "skid", "🐤")
+
+st.write("Explicit `st.write`:", (6, 6, "skid", "🐤"))
+
+# Lol. Why is this different ?
+
+"""Ah, I see; since streamlit wants to be able to do magic on multiple element,
+it *interprets* literal tuples, not as an object, but as an instruction to 
+magically `st.write` multiple things."""
+
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
